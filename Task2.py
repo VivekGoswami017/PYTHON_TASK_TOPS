01e2124186adc983c8431c690b2a26832600b62c
Vivek Goswami17 <vivekgoswamivg336278@gmail.com>
2026-07-03 18:58:37 +0530

    Refactor comments in Task2.py for clarity
    Updated comments to improve readability and consistency.

--- Task2.py
+++ Task2.py
@@ -1,4 +1,4 @@
-# QUESTION1: Create three variables in Python named item_price, delivery_fee, and coupon_discount, and assign them values representing a Swiggy order (e.g., 250, 30, 20) Print each variable.
+# Question : 1  Create three variables in Python named item_price, delivery_fee, and coupon_discount, and assign them values representing a Swiggy order (e.g., 250, 30, 20) Print each variable.
 
 # Swiggy _order_details:
 
@@ -10,10 +10,9 @@ print(f"Item Price:{item_price}")
 print(f"Delivery Fee:{ delivery_fee}")
 print(f"Coupon Discount:{coupon_discount}")
 
-# QUESTION2: Write a Python script that uses indentation correctly to check if a Flipkart product's price is above 1000; if yes, print 'Eligible for free delivery', else print 'Delivery charges apply'.<br><br><em><strong>Hint:</strong> Use an if-else block and make sure your indentation is consistent.</em>
+# Question : 2  Write a Python script that uses indentation correctly to check if a Flipkart product's price is above 1000; if yes, print 'Eligible for free delivery', else print 'Delivery charges apply'.<br><br><em><strong>Hint:</strong> Use an if-else block and make sure your indentation is consistent.</em>
 
 # Flipkart_product_price
-
 product_price =int(input("enter product price"))
 if product_price > 1000:
     print("Eligible for free delivery")
@@ -21,7 +20,7 @@ else:
     print("Delivery charges apply")
 
 
-# QUESTION3:Add both single-line and multi-line comments in your script explaining what each section does, using # for single-line and triple quotes for multi-line comments.
+# Question : 3  Add both single-line and multi-line comments in your script explaining what each section does, using # for single-line and triple quotes for multi-line comments.
 
 # This line is defining single line comment and his program demonstrates variables, comments, and if-else statements.
 
@@ -32,7 +31,7 @@ The program uses Swiggy and Flipkart examples.
 
 """ 
 
-# QUESTION4: Create variables for an IPL match: team_name, runs_scored, and overs_played. Assign appropriate values and print them using the correct naming conventions for Python variables.<br><br><em><strong>Constraint:</strong> Use snake_case for all variable names.</em>
+# Question : 4  Create variables for an IPL match: team_name, runs_scored, and overs_played. Assign appropriate values and print them using the correct naming conventions for Python variables.<br><br><em><strong>Constraint:</strong> Use snake_case for all variable names.</em>
 
 # IPL _match_details
 team_name = "RCB"
